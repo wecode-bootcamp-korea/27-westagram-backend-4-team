81f13ae0a454337bd27488728698dfb0adf4bf87
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-11-18 16:38:33 +0900

    Modified: DATABASES removed

--- students/junghyun/westagram/settings.py
+++ students/junghyun/westagram/settings.py
@@ -82,13 +82,7 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
