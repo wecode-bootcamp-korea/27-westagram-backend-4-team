286021e67ddc9625850c59d370aa459323b687c1
jaemoon <jaemoon406@gmail.com>
2021-11-18 18:47:22 +0900

    Modify: same convention

--- students/jaemoon/users/models.py
+++ students/jaemoon/users/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name     = models.CharField(max_length=20)
-    email    = models.CharField(max_lenght=45,unique=True)
-    password = models.IntegerField(max_lenght=200)
-    call_num = models.IntegerField(max_lenght=45)
-    address = models.CharField(max_lenght=50)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    name        = models.CharField(max_length=20)
+    email       = models.CharField(max_lenght=45,unique=True)
+    password    = models.IntegerField(max_lenght=200)
+    call_num    = models.IntegerField(max_lenght=45)
+    address     = models.CharField(max_lenght=50)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table='users'
