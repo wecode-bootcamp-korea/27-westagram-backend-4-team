352d2a3819cd07c5274b9a8f7a1c1ad730348fed
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-18 17:30:29 +0900

    my_setting모듈 위로 올림/from django.contrib import admin 삭제

--- students/jongho/westagram/settings.py
+++ students/jongho/westagram/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from my_settings import DATABASES,SECRET_KEY
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'westagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-from my_settings import DATABASES,SECRET_KEY
+
 DATABASES=DATABASES
 SECRET_KEY=SECRET_KEY
 
