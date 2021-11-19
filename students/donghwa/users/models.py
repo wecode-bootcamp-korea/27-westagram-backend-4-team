30cc269e6e1b604e47347a30b08a9f4cbdb328c7
dk-sync <donghwa2428@gmail.com>
2021-11-19 16:37:44 +0900

    models.py email&pw modified

--- students/donghwa/users/models.py
+++ students/donghwa/users/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class User(models.Model):
     name        = models.CharField(max_length=10)
-    email       = models.CharField(max_length=45)
-    password    = models.CharField(max_length=30)
-    contacts    = models.IntegerField(max_length=11)
+    email       = models.EmailField(max_length=45, unique = True)
+    password    = models.CharField(max_length=200)
+    contacts    = models.IntegerField(max_length=20)
     address     = models.CharField(max_length=100)
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_tables = 'users'
+        db_table = 'users'
 
 
