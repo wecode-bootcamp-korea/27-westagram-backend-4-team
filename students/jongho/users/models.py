36995f444070e1a01dee03592ded3df3c136dffb
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-19 16:06:28 +0900

    models meta 추가

--- students/jongho/users/models.py
+++ students/jongho/users/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class User(models.Model):
-    
     id         =models.AutoField(primary_key=True)
     name       =models.CharField(max_length=100)
     email      =models.EmailField(unique=True)
@@ -10,4 +9,11 @@ class User(models.Model):
     information=models.TextField()
     created_at =models.DateTimeField(auto_now_add=True)
     updated_at =models.DateTimeField(auto_now=True)
+
+    class Meta():
+      db_table="users"
+    
+    def __str__(self):
+      return self.name
+
 # Create your models here.
