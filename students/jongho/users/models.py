44cf164f38644c4d06c57e0e9b7f88950c70181d
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-19 13:24:12 +0900

    users-models 작성완료상태

--- students/jongho/users/models.py
+++ students/jongho/users/models.py
@@ -9,6 +9,4 @@ class User(models.Model):
     information=models.TextField()
     created_at =models.DateTimeField(auto_now_add=True)
     updated_at =models.DateTimeField(auto_now=True)
-
-
 # Create your models here.
