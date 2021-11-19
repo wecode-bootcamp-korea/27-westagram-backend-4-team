d8697b9ec0c6adbf3c8fb9417d521ed7e7db5de8
dk-sync <donghwa2428@gmail.com>
2021-11-19 14:17:14 +0900

    models.py modified

--- students/donghwa/users/models.py
+++ students/donghwa/users/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    Name        = models.CharField(max_length=10)
-    Email       = models.CharField(max_length=45)
-    Password    = models.CharField(max_length=30)
-    Contacts    = models.IntegerField(max_length=11)
-    Address     = models.CharField(max_length=100)
+    name        = models.CharField(max_length=10)
+    email       = models.CharField(max_length=45)
+    password    = models.CharField(max_length=30)
+    contacts    = models.IntegerField(max_length=11)
+    address     = models.CharField(max_length=100)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_tables = 'users'
+
+
