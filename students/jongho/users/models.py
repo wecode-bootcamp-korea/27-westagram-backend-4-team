f5a2f773b8a65ea833780fc9891d7f90fb862506
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-19 13:23:17 +0900

    users-models 작성완료상태

---
+++ students/jongho/users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+class User(models.Model):
+    
+    id         =models.AutoField(primary_key=True)
+    name       =models.CharField(max_length=100)
+    email      =models.EmailField(unique=True)
+    password   =models.IntegerField(max_length=200)
+    contact    =models.CharField(max_length=100)
+    information=models.TextField()
+    created_at =models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
+
+
+# Create your models here.
