c46035ebf6a372f482dadb170df76aeffdc4993e
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-22 15:43:16 +0900

    users-view modified

--- students/jongho/users/views.py
+++ students/jongho/users/views.py
@@ -10,11 +10,13 @@ class UserInformaiton(View):
   def post(self,request):
     try:
       data                   = json.loads(request.body)
-      email_confirm_regex    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+      email_confirm_regex    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]{2,3}+$'
       password_confirm_regex = '^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$'
       user_unique_confirm    = User.objects.filter(email=data["email"])
 
-      if re.match(email_confirm_regex,data["email"]) and re.match(password_confirm_regex,data["password"]) and user_unique_confirm.exists()==False:
+      if (re.match(email_confirm_regex,data["email"]) and 
+          re.match(password_confirm_regex,data["password"]) and 
+          user_unique_confirm.exists()==False):
         User.objects.create(
           name        = data["name"],
           email       = data["email"],
