7468511cd2760965dd1b59a23c7c9bfb5a378c75
dk-sync <donghwa2428@gmail.com>
2021-11-22 16:52:09 +0900

    added loginview

--- students/donghwa/users/views.py
+++ students/donghwa/users/views.py
@@ -42,3 +42,21 @@ class SignUpView(View):
 
         except KeyError:
             return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+
+
+class LoginView(View):
+      def post(self, request):
+          data     = json.loads(request.body)
+          email    = User.objects.filter(email = data['email'])
+          password = User.objects.filter(password = data['password'])
+
+          try:
+
+              if email.exists() and password.exists():
+                  return JsonResponse({"message" : "SUCCESS"}, status=200)
+
+              else:
+                  return JsonResponse({"message" : "INVALID_USER"}, status=401)
+
+          except KeyError:
+              return JsonResponse({"message" : "KEY_ERROR"}, status=400)
