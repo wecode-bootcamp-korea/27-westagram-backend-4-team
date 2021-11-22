64ba4e9e5bb628590b75a6d2ddaddd14c8dddc85
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-11-22 10:23:28 +0900

    Modified fieldname and Deleted unnecessary imports

--- students/junghyun/users/models.py
+++ students/junghyun/users/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-	name        = models.CharField(max_length=20)
-	email       = models.CharField(max_length=100, unique=True)
-	password    = models.CharField(max_length=200)
-	phonenumber = models.CharField(max_length=20)
-	information = models.CharField(max_length=200, null=True)
-	created_at  = models.DateTimeField(auto_now_add=True)
-	updated_at  = models.DateTimeField(auto_now=True)
+	name         = models.CharField(max_length=20)
+	email        = models.CharField(max_length=100, unique=True)
+	password     = models.CharField(max_length=200)
+	phone_number = models.CharField(max_length=20)
+	information  = models.CharField(max_length=200, null=True)
+	created_at   = models.DateTimeField(auto_now_add=True)
+	updated_at   = models.DateTimeField(auto_now=True)
 
 	class Meta:
 		db_table = 'users'
--- students/junghyun/users/views.py
+++ students/junghyun/users/views.py
@@ -1,9 +1,9 @@
 import re, json
 
-from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.core.exceptions import ValidationError
 from django.http            import JsonResponse
 from django.views           import View
-from django.db              import IntegrityError, DataError
+from django.db              import DataError
 
 from .models                import User    
 
@@ -25,15 +25,15 @@ class SignUpView(View):
             User.objects.create(
                 name         = data["name"],
                 email        = data["email"],
-                phonenumber  = data["phonenumber"],
+                phone_number  = data["phone_number"],
                 password     = data["password"],
                 information  = data["info"],
              )
 
             return JsonResponse({"message" : "SUCCESS"}, status=201) 
         
-        except ValidationError as e:
-            return JsonResponse({"message":e.message}, status=401)
+        except ValidationError:
+            return JsonResponse({"message":"INVALID_ERROR"}, status=401)
             
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"}, status=400)
