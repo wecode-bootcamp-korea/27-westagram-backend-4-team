ce9465d16b4a13ebb4f4e37b1a3988d55161ef47
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-23 10:59:05 +0900

    users-view modified

--- students/jongho/users/urls.py
+++ students/jongho/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import SignUp
+from .views import SignUpView
 
 urlpatterns = [
-    path("/signup",SignUp.as_view())
+    path("/signup",SignUpView.as_view())
 ]
