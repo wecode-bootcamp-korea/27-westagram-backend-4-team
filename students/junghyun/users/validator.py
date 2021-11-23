086f17e0ce6082b61f341ae68146bac323e746fa
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-11-23 14:50:08 +0900

    Modified: changed variables' name

--- students/junghyun/users/validator.py
+++ students/junghyun/users/validator.py
@@ -3,11 +3,11 @@ import re
 from django.core.exceptions import ValidationError
 
 def email_regex_match(email):   
-    email_regex = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    if not re.match(email_regex, email):
+    EMAIL_REGEX = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    if not re.match(EMAIL_REGEX, email):
         raise ValidationError("EMAIL_INVALID")
     
 def password_regex_match(password):   
-    password_regex = "^.*(?=^.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%*^&+=]).*$"
-    if not re.match(password_regex, password):
+    PASSWORD_REGEX = "^.*(?=^.{8,}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%*^&+=]).*$"
+    if not re.match(PASSWORD_REGEX, password):
         raise ValidationError("PASSWORD_INVALID")
