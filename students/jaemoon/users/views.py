3829d9fd7e37c9e845a5b4dc46fbd199172b68db
jaemoon <jaemoon406@gmail.com>
2021-11-23 10:11:45 +0900

    [Modify] deletecomment

--- students/jaemoon/users/views.py
+++ students/jaemoon/users/views.py
@@ -23,11 +23,3 @@ class SignupView(View):
             return JsonResponse({"message": "CHECK_VALUE"})
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"},status=400)
-    '''
-    1. 이름 이메일 비번 정보 입력 
-    2. 이메일 or 비번 정보 전달 x {"message": "KEY_ERROR"}, status code 400 반환
-    3. 이메일 @ . 정규식 필수 포함
-    4. 비번 8자리 이상 - 문자 숫자 특수문자 복합
-    5. 이메일 중복 x
-    6. 회원가입 성공 시 DB저장 및 {"message": "SUCCESS"}, status code 201 반환
-    '''
