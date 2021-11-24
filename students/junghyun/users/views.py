8e90938421412d228c6f264ba6967d1597f86ed1
Hailey Park <haileypark@Haileys-MacBook-Pro.local>
2021-11-24 10:16:14 +0900

    Modifie

--- students/junghyun/users/views.py
+++ students/junghyun/users/views.py
@@ -1,4 +1,4 @@
-import json, bcrypt, jwt
+import json, bcrypt
 
 from django.core.exceptions import ValidationError
 from django.http            import JsonResponse
@@ -12,9 +12,11 @@ from .validator             import email_regex_match, password_regex_match
 class SignUpView(View):
     def post(self, request):
         try:
-            data           = json.loads(request.body)
-            email          = data["email"]
-            password	   = data["password"]
+            data          = json.loads(request.body)
+            name          = data["name"]
+            email         = data["email"]
+            password	  = data["password"]
+            phone_number  = data["phone_number"]
             
             email_regex_match(email)
             password_regex_match(password)
@@ -22,11 +24,13 @@ class SignUpView(View):
             if User.objects.filter(email=email).exists():
                 return JsonResponse({"message":"USER_ALREADY_EXISTS"}, status = 401)
             
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode()
+            
             User.objects.create(
-                name          = data["name"],
+                name          = name,
                 email         = email,
-                password      = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode(),
-                phone_number  = data["phone_number"],
+                password      = hashed_password,
+                phone_number  = phone_number,
                 information   = data.get("info"),
              )
 
@@ -48,10 +52,8 @@ class SignInView(View):
             password = User.objects.get(email=data['email']).password
             if not bcrypt.checkpw(data['password'].encode('utf-8'), password.encode('utf-8')):
                 return JsonResponse({'message': 'INVALID_PASSWORD'}, status = 401)
-            return JsonResponse({'message':'SUCCESS',
-                                'access_token' : jwt.encode({'id' : 1}, SECRET_KEY, algorithm = 'HS256')
-                                }, status = 200)
-        
+            return JsonResponse({'message':'SUCCESS'}, status = 200)
+                           
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'},status = 400) 
         
