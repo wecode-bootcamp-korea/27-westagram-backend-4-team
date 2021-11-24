13ec1b4f0342c14859fe5c180d19145b7b9b652f
jaemoon <jaemoon406@gmail.com>
2021-11-24 21:21:14 +0900

    [add] coded_password

--- students/jaemoon/users/views.py
+++ students/jaemoon/users/views.py
@@ -1,4 +1,4 @@
-import json, re
+import json, re, bcrypt
 
 from django.http  import JsonResponse
 from django.views import View
@@ -9,21 +9,21 @@ from users.models import User
 class SignupView(View):
     def post(self,request):
         try:
-            data           = json.loads(request.body)
-            email_regex    = r'^[a-zA-Z0-9.-_]+\@[a-zA-Z0-9.-]+\.[a-zA-Z]+$'
-            password_regex = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}'
-            
+            data            = json.loads(request.body)
+            email_regex     = r'^[a-zA-Z0-9.-_]+\@[a-zA-Z0-9.-]+\.[a-zA-Z]+$'
+            password_regex  = r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}'
+            hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt())
             if not re.match(email_regex,data['email']):
                 return JsonResponse({'message':'INVALID_EMAIL'}, status=400)
             if not re.match(password_regex,data['password']):
                 return JsonResponse({'message':'INVALID_PASSWORD'}, status=400)
             
             User.objects.create(
-                name          = data['name'],
-                email         = data['email'],
-                password      = data['password'],
-                phone_number  = data['phone_number'],
-                address       = data.get('address','')
+                name            = data['name'],
+                email           = data['email'],
+                password        = hashed_password.decode('utf-8'),
+                phone_number    = data['phone_number'],
+                address         = data.get('address',''),
                 )
             return JsonResponse({'message':'CREATE'}, status=201)
         
@@ -35,10 +35,9 @@ class LoginView(View):
         try:
             data = json.loads(request.body)
             user = User.objects.get(email=data['email'])
-            
-            if not user:
-                return JsonResponse({'message': 'INVALID_USER'}, status=400)
-            if user.password == data['password']:
+            hashed_password =bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt())
+
+            if bcrypt.checkpw(data["password"].encode('utf-8'),user.password.encode('utf-8')):
                 return JsonResponse({'message':'SUCCESS'},status=200)
             return JsonResponse({'message':'INVALID_USER'},status=401)
 
