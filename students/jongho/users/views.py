7a04dd65c86a04082a239c831f7bc7bbc0e58152
JongHo Seong <jonghoseong@JongHoui-MacBookAir.local>
2021-11-24 09:49:31 +0900

    views-signin modified

--- students/jongho/users/views.py
+++ students/jongho/users/views.py
@@ -5,13 +5,14 @@ from django.http            import JsonResponse
 from django.db              import DataError
 from django.core.exceptions import ValidationError
 
-from .models      import User
-from .validation   import email_regex,password_regex
+from .models                import User
+from .validation            import email_regex,password_regex
 
 class SignUpView(View):
   def post(self,request):
     try:
       data = json.loads(request.body)
+
       email_regex(data)
       password_regex(data)
       
@@ -25,25 +26,22 @@ class SignUpView(View):
       phone_number = data["phone_number"],
       information  = data.get("information","")
     )
-      return JsonResponse({'message':"CREATE"},status=201)
+      return JsonResponse({'message':"CREATE"},status = 201)
 
-    except KeyError             : return JsonResponse({'message':"KEYERROR"},status=400)
-    except DataError            : return JsonResponse({"message":"DATAERROR"},status=401)
-    except ValidationError as e : return JsonResponse({"message":e.message},status=401)
+    except KeyError             : return JsonResponse({'message':"KEYERROR"},status = 400)
+    except DataError            : return JsonResponse({"message":"DATAERROR"},status = 401)
+    except ValidationError as e : return JsonResponse({"message":e.message},status = 401)
       
 class SignInView(View):
   def post(self,request):
     try:
       data = json.loads(request.body)
-      email_regex(data) #DB hit전에 정규표현식 검사
-      password_regex(data)
       user = User.objects.get(email=data["email"])
 
       if data["password"] == user.password and data["email"] == user.email:
         return JsonResponse({'message':"SUCCESS"},status=200)
 
-      return JsonResponse({'message':"INVALID_PASSWORD"},status=401)
+      return JsonResponse({'message':"INVALID_USER"},status=401)
 
-    except KeyError             : return JsonResponse({'message':"KEYERROR"},status=400)
-    except User.DoesNotExist    : return JsonResponse({'message':"INVALID_EMAIL"},status=401)
-    except ValidationError as e : return JsonResponse({"message":e.message},status=401)
+    except KeyError             : return JsonResponse({'message':"KEYERROR"},status = 400)
+    except User.DoesNotExist    : return JsonResponse({'message':"INVALID_USER"},status = 401)
